2fb0954c7b989855b40e657e64c6f70a0c603080
Anastasia Simolova <simolovaai@gmail.com>
2022-11-27 00:50:32 +0300

    create tests

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,32 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void catGetSound() {
+        Cat cat = new Cat (feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void catGetFood() throws Exception {
+        Cat cat = new Cat (feline);
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expected);
+        assertEquals(expected, cat.getFood());
+    }
+}
